5f5db9bde3da66b4470264f225b2beaacd4587fc
Snex-Thadeus <lwande94@gmail.com>
2020-09-16 13:42:35 +0300

    Models and user authentication

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,19 @@
-from . import views
 from django.urls import path
+from . import views
+
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('register/', views.registerPage, name="register"),
+    path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
+
+    path('', views.home, name="home"),
     path('product/', views.products, name='product'),
-    path('customer/<str:pk_test>/', views.customer, name='customer'),
+    path('customer/<str:pk_test>/', views.customer, name="customer"),
+
+    path('create_order/', views.createOrder, name="create_order"),
+    path('update_order/<str:pk>/', views.updateOrder, name="update_order"),
+    path('delete_order/<str:pk>/', views.deleteOrder, name="delete_order"),
+
+
 ]
